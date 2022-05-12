c353c53c9a8caecde6e6bdcbe1ceaea2a8e92afb
Alexandre Sapata Carbonell <alexandrenescau@gmail.com>
2022-05-11 22:09:10 -0300

    update content on hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -13,14 +13,12 @@ const Hero = () => {
         />
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            Designer gráfico, Criação de Materiais, Gestão de Mídias e propostas
-            criativas
+            Direção criativa para um mundo mais conectado.
           </h1>
           <p class="mb-8 leading-relaxed">
             Me chamo Ingrid Fischer e sou apaixonada pelo que faço. Criativa,
-            amo explorar e trazer para meus clientes e empresas mais humanidade
-            no conteúdo digital. Em constante mudança, o Marketing Digital já se
-            faz o futuro-presente.
+            amo explorar e trazer para os meus clientes e empresas mais
+            humanidade e conteúdo de relevância no cenário digital.
           </p>
           <div class="flex justify-center">
             <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
