38556832074a41fab169f29ff1722f5a6d949b7c
Alexandre Sapata Carbonell <alexandrenescau@gmail.com>
2022-05-12 09:08:06 -0300

    add images on gallery

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,4 +1,7 @@
 import * as React from "react"
+import image1 from "../images/Imagem-1-820x340.jpg"
+import image2 from "../images/Imagem-2-542x420.jpg"
+import image3 from "../images/Imagem-3-542x420.jpg"
 
 import "./layout.css"
 
@@ -10,19 +13,19 @@ const Gallery = () => {
           <div class="flex flex-wrap w-full bg-gray-100 py-32 px-10 relative mb-4">
             <img
               alt="gallery"
-              class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-              src="https://dummyimage.com/820x340"
+              class="w-full object-cover h-full object-center block opacity-50 absolute inset-0"
+              src={image1}
             />
             <div class="text-center relative z-10 w-full">
               <h2 class="text-2xl text-gray-900 font-medium title-font mb-2">
-                Shooting Stars
+                SHOOTINGS
               </h2>
               <p class="leading-relaxed">
-                Skateboard +1 mustache fixie paleo lumbersexual.
+                {/* Skateboard +1 mustache fixie paleo lumbersexual. */}
               </p>
               <a class="mt-3 text-indigo-500 inline-flex items-center">
-                Learn More
-                <svg
+                {/* Learn More */}
+                {/* <svg
                   fill="none"
                   stroke="currentColor"
                   stroke-linecap="round"
@@ -32,7 +35,7 @@ const Gallery = () => {
                   viewBox="0 0 24 24"
                 >
                   <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
+                </svg> */}
               </a>
             </div>
           </div>
@@ -41,19 +44,19 @@ const Gallery = () => {
               <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
                 <img
                   alt="gallery"
-                  class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-                  src="https://dummyimage.com/542x460"
+                  class="w-full object-cover h-full object-center block opacity-50 absolute inset-0"
+                  src={image2}
                 />
                 <div class="text-center relative z-10 w-full">
                   <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
-                    Shooting Stars
+                    LOGOS & IDENTIDADES
                   </h2>
                   <p class="leading-relaxed">
-                    Skateboard +1 mustache fixie paleo lumbersexual.
+                    {/* Skateboard +1 mustache fixie paleo lumbersexual. */}
                   </p>
                   <a class="mt-3 text-indigo-500 inline-flex items-center">
-                    Learn More
-                    <svg
+                    {/* Learn More */}
+                    {/* <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
@@ -63,7 +66,7 @@ const Gallery = () => {
                       viewBox="0 0 24 24"
                     >
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
+                    </svg> */}
                   </a>
                 </div>
               </div>
@@ -72,19 +75,19 @@ const Gallery = () => {
               <div class="flex flex-wrap w-full bg-gray-100 sm:py-24 py-16 sm:px-10 px-6 relative">
                 <img
                   alt="gallery"
-                  class="w-full object-cover h-full object-center block opacity-25 absolute inset-0"
-                  src="https://dummyimage.com/542x420"
+                  class="w-full object-cover h-full object-center block opacity-50 absolute inset-0"
+                  src={image3}
                 />
                 <div class="text-center relative z-10 w-full">
                   <h2 class="text-xl text-gray-900 font-medium title-font mb-2">
-                    Shooting Stars
+                    SOCIAL MEDIA
                   </h2>
                   <p class="leading-relaxed">
-                    Skateboard +1 mustache fixie paleo lumbersexual.
+                    {/* Skateboard +1 mustache fixie paleo lumbersexual. */}
                   </p>
                   <a class="mt-3 text-indigo-500 inline-flex items-center">
-                    Learn More
-                    <svg
+                    {/* Learn More */}
+                    {/* <svg
                       fill="none"
                       stroke="currentColor"
                       stroke-linecap="round"
@@ -94,7 +97,7 @@ const Gallery = () => {
                       viewBox="0 0 24 24"
                     >
                       <path d="M5 12h14M12 5l7 7-7 7"></path>
-                    </svg>
+                    </svg> */}
                   </a>
                 </div>
               </div>
