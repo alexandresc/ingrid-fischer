0d252fa3a227bef68d1a321b8e92e89487aea57e
Alexandre Sapata Carbonell <alexandrenescau@gmail.com>
2022-05-11 22:10:03 -0300

    update content content

--- src/components/content.js
+++ src/components/content.js
@@ -12,12 +12,11 @@ const Content = () => {
           </div>
           <div class="flex flex-wrap sm:flex-row flex-col py-6 mb-12">
             <h1 class="sm:w-2/5 text-gray-900 font-medium title-font text-2xl mb-2 sm:mb-0">
-              Space The Final Frontier
+              Projetos mais recentes
             </h1>
             <p class="sm:w-3/5 leading-relaxed text-base sm:pl-10 pl-0">
-              Street art subway tile salvia four dollar toast bitters selfies
-              quinoa yuccie synth meditation iPhone intelligentsia prism tofu.
-              Viral gochujang bitters dreamcatcher.
+              Acompanhe os projetos mais recentes e artigos sobre o marketing
+              digital!
             </p>
           </div>
         </div>
